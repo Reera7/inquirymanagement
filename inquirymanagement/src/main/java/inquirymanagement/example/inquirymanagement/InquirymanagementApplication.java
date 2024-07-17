6082a41d7f0c99cee061b663e074cdcee86e5a22
Reera <aigis.goddess@outlook.jp>
2024-07-18 00:22:31 +0900

    プロジェクト作成：application.propertiesやpom.xmlの追記

---
+++ inquirymanagement/src/main/java/inquirymanagement/example/inquirymanagement/InquirymanagementApplication.java
@@ -0,0 +1,13 @@
+package inquirymanagement.example.inquirymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquirymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquirymanagementApplication.class, args);
+	}
+
+}
---
+++ inquirymanagement/src/test/java/inquirymanagement/example/inquirymanagement/InquirymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package inquirymanagement.example.inquirymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquirymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
